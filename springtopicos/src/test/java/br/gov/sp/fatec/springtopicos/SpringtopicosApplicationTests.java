068ab9818d88a2dd89983078715010ea84909fad
Érick Hideki Oyakawa Awata <erickhoawata@gmail.com>
2025-09-09 09:54:47 -0300

    feat: inicio

---
+++ springtopicos/src/test/java/br/gov/sp/fatec/springtopicos/SpringtopicosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springtopicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtopicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
